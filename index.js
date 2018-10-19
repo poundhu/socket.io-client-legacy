a621c00ba23c39db93d458a58761a5c8428dead8
poundhu <hupanbieku@163.com>
2018-10-19 22:49:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-module.exports = require('socket.io-client');
+'use strict';
+module.exports = {
+	io: require('socket.io'),
+	emitter: require('socket.io-emitter'),
+	redis: require('socket.io-redis'),
+}
